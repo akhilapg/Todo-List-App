efe2192de1fb8efab01a9f3979b7286bdb461498
Akhila <akhilapg637@gmail.com>
2026-02-23 17:16:29 +0530

    created pages

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,6 +8,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,16 +22,62 @@ class _HomeScreenState extends State<HomeScreen> {
             backgroundImage: AssetImage('assets/images/profile_pic'),
             radius: 25.0,
           ),
-          SizedBox(width: 15,),
+          SizedBox(width: 15),
         ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("What do you want to do today?",style: TextStyle(fontSize: 15),),
-            SizedBox(height: 10,),
-            Text("Tap + to add yor tasks",style: TextStyle(fontSize: 10),)
+            Text(
+              "What do you want to do today?",
+              style: TextStyle(fontSize: 15),
+            ),
+            SizedBox(height: 10),
+            Text("Tap + to add yor tasks", style: TextStyle(fontSize: 10)),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xFF6C63FF),
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar:
+      BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child:
+      BottomNavigationBar(backgroundColor: Colors.black26,
+          currentIndex: selectedIndex,
+          onTap: (index) {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+          selectedLabelStyle: TextStyle(
+            fontSize: 10,
+            fontWeight: FontWeight.bold,
+          ),
+          unselectedLabelStyle: TextStyle(fontSize: 8),
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 15),
+              label: "Index",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today, size: 15),
+              label: "Calender",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.timer, size: 15),
+              label: "Focuse",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person, size: 15),
+              label: "Profile",
+            ),
           ],
         ),
       ),
